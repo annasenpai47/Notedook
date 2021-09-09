0bd064f768eb8d40dcfcd02eb453aadd21c425db
annasenpai47 <annasenpai47@gmail.com>
2021-09-10 00:28:33 +0300

    add InputNewNotebook

--- src/Notebook/controller/InputNewNotebook.java
+++ src/Notebook/controller/InputNewNotebook.java
@@ -1,5 +1,28 @@
 package Notebook.controller;
 
+import Notebook.view.View;
+
+import java.util.Scanner;
+
+import static Notebook.controller.RegexForInput.*;
+import static Notebook.view.TextConstant.*;
+
 public class InputNewNotebook {
+    private View view;
+    private Scanner sc;
+
+    CorrectInput utilityController = new CorrectInput(sc, view);
+
+    public InputNewNotebook(View view, Scanner sc) {
+        this.view = view;
+        this.sc = sc;
+    }
+
+    public void inputName() {
+        String s = utilityController.inputStringValueWithScanner(SURNAME, NAMES);
+        view.printSMTH(s);
+
+    }
+
 
 }
