88d7aa3bd941702a688ebed719c9111dc5c5671d
annasenpai47 <annasenpai47@gmail.com>
2021-09-10 00:31:47 +0300

    rebuild View

--- src/Notebook/view/View.java
+++ src/Notebook/view/View.java
@@ -1,11 +1,37 @@
 package Notebook.view;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static Notebook.view.TextConstant.*;
+
 public class View {
 
+    private static String MESSAGES_BUNDLE_NAME = "message";
+    public static final ResourceBundle bundle =
+            ResourceBundle.getBundle(
+                    MESSAGES_BUNDLE_NAME,
+                    //new Locale("ua", "UA"));  // Ukrainian
+                    new Locale("en"));
 
-    public void entrSMTH(String smth){
-        System.out.println(smth+"ssss");
+    public void printSMTH(String message){
+        System.out.println(message);
+    }
+
+    private String concatenationString(String... message){
+        StringBuilder concatString = new StringBuilder();
+        for(String v : message) {concatString.append(v);}
+        return new String(concatString);
     }
 
 
+    public void printStringInput(String message) {
+        printSMTH(bundle.getString(message));
+    }
+
+    public void printWrongStringInput(String message) {
+        printSMTH(concatenationString(
+                bundle.getString(WRONG_DATA),
+                bundle.getString(message)));
+    }
 }
