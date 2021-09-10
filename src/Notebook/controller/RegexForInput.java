4a74fb513a9d99b85304785be4a892c6b64f581d
annasenpai47 <annasenpai47@gmail.com>
2021-09-10 16:12:39 +0300

    rebuild regex for tel

--- src/Notebook/controller/RegexForInput.java
+++ src/Notebook/controller/RegexForInput.java
@@ -1,16 +1,18 @@
 package Notebook.controller;
 
+import Notebook.view.View;
+
 public interface RegexForInput {
 
-    //String NAMES = "input.regex.name";
-    String NAMES = "[\\d]*";
-    String UNNUL = "[\\D]+.*";
-    String NUM_COUSE = "[\\d]{1}";
-    String DOM_TEL = "[\\d]{6}";
-    String MOD_TEL = "[+]?([3]{1}[8]{1})?[0]{1}[\\d]{9}";
-    String EMAIL = "[\\d\\D]+[@]{1}[\\D]+[.]{1}[\\D]+";
-    String SKYPE = "[a-zA-Z]{3,}";
-    String INDEX = "[\\d]{5}";
-    String DOM = "[\\d]+.*";
-    String KV = "[^0][\\d]+";
+    String NAMES_REGEX = View.bundle.getString("input.regex.name");
+    String NICK_REGEX = "[A-Za-z0-9]{5,20}";
+    String UNNUL_REGEX = ".*";
+    String COUSE_REGEX = "[1-6]{1}";
+    String DOM_TEL_REGEX = "[\\d]{6}";
+    String MOD_TEL_REGEX = "([+]?[3]{1}[8]{1})?[0]{1}[\\d]{9}";
+    String EMAIL_REGEX = "[\\d\\D]+[@]{1}[\\D]+[.]{1}[\\D]+";
+    String SKYPE_REGEX = "[a-zA-Z]{3,}";
+    String INDEX_REGEX = "[\\d]{5}";
+    String DOM_REGEX = "[\\d]+.*";
+    String KV_REGEX = "[^0][\\d]+";
 }
