3e2ed051542bd6bdd3f97b1cb815dd22af506253
annasenpai47 <annasenpai47@gmail.com>
2021-09-10 16:09:58 +0300

    correct registration

--- src/Notebook/Main.java
+++ src/Notebook/Main.java
@@ -10,5 +10,6 @@ public class Main {
 
         Controller controller = new Controller(new Model(), new View());
         controller.registration();
+
     }
 }
--- src/Notebook/controller/Controller.java
+++ src/Notebook/controller/Controller.java
@@ -1,11 +1,9 @@
 package Notebook.controller;
 
-import Notebook.model.Group;
 import Notebook.model.Model;
 import Notebook.view.View;
 
 import java.util.Scanner;
-import static Notebook.view.TextConstant.*;
 
 
 
@@ -13,18 +11,6 @@ public class Controller {
 
     private Model model;
     private View view;
-
-    private static final String NAMES = "[а-яА-Яa-zA-Z]{2,}[-]?[а-яА-Яa-zA-Z]*\\s*";
-    private static final String UNNUL = "[\\D]+.*";
-    private static final String NUM_COUSE = "[\\d]{1}";
-    private static final String DOM_TEL = "[\\d]{6}";
-    private static final String MOD_TEL = "[+]?([3]{1}[8]{1})?[0]{1}[\\d]{9}";
-    private static final String EMAIL2 = "[\\d\\D]+[@]{1}[g]?[m]{1}[a]{1}[i]{1}[l]{1}[.]{1}(([c]{1}[o]{1}[m]{1})?([r]{1}(u){1})?){1}]";
-    private static final String EMAIL = "[\\d\\D]+[@]{1}[\\D]+[.]{1}[\\D]+";
-    private static final String SKYPE = "[a-zA-Z]{3,}";
-    private static final String INDEX = "[\\d]{5}";
-    private static final String DOM = "[\\d]+.*";
-    private static final String KV = "[^0][\\d]+";
 
     Scanner sc = new Scanner(System.in);
 
@@ -36,146 +22,19 @@ public class Controller {
     public void registration(){
         InputNewNotebook inputNewNotebook = new InputNewNotebook(view,sc);
 
-
-    }
-/*
-    public void startProgram(){
-        enterName();
-        enterAddInfo();
-        enterConnect();
-        enterAddress();
+        String [] s = inputNewNotebook.inputName();
+        model.setFIO(s[0], s[1], s[2]);
+        model.setNick(inputNewNotebook.inputNick());
+        model.setCom(inputNewNotebook.inputCom());
+        model.setGroup(inputNewNotebook.inputCroup(), inputNewNotebook.inputCourse());
+        model.setDom_tel(inputNewNotebook.inputDomTel());
+        model.setMod_tel(inputNewNotebook.inputMobTel());
+        model.setMod_tel_add(inputNewNotebook.inputDopMobTel());
+        model.setEmail(inputNewNotebook.inputEmail());
+        model.setSkype(inputNewNotebook.inputSkype());
+        model.setAddress(inputNewNotebook.inputIndex(), inputNewNotebook.inputCity(),
+                inputNewNotebook.inputStreet(),inputNewNotebook.inputDom(),inputNewNotebook.inputApart());
         model.setDate();
-        model.printInfo();
-
-
-
-
-    }*/
-/*
-    private void enterName(){
-        String surname, name, batko;
-        view.printSMTH(SURNAME);
-        while (!(surname = sc.nextLine()).matches(NAMES)) {
-            view.printSMTH(WRONG_DATA);
-        }
-        view.printSMTH(NAME);
-        while (!(name = sc.nextLine()).matches(NAMES)) {
-            view.printSMTH(WRONG_DATA);
-        }
-        view.printSMTH(BATKO);
-        while (!(batko = sc.nextLine()).matches(NAMES)) {
-            view.printSMTH(WRONG_DATA);
-        }
-        model.setFIO(surname, name, batko);
+        view.printSMTH(model.getInfo());
     }
-
-    private void enterAddInfo(){
-        String nick, com, gr, c;
-        Group group = null;
-        boolean loop = true;
-        view.printSMTH(NICK);
-        while (!(nick = sc.nextLine()).matches(UNNUL)) {
-            view.printSMTH(WRONG_DATA);
-        }
-        model.setNick(nick);
-        view.printSMTH(COM);
-        com = sc.nextLine();
-        model.setCom(com);
-        view.printSMTH(GROUP);
-        while (loop){
-            gr = sc.nextLine();
-            switch (gr){
-                case "AND":
-                    group = Group.AND;
-                    loop = false;
-                    break;
-                case "PP":
-                    group = Group.PP;
-                    loop = false;
-                    break;
-                case "KN":
-                    group = Group.KN;
-                    loop = false;
-                    break;
-                default:
-                    view.printSMTH(WRONG_GROUP);
-                    for (Group el: Group.values()){
-                        System.out.print(el + " ");
-                    }
-                    System.out.println();
-                    break;
-            }
-        }
-        view.printSMTH(COURSE);
-        while (!((c = sc.nextLine()).matches(NUM_COUSE) && (Integer.parseInt(c) > 0 && Integer.parseInt(c) < 7 ))) {
-            view.printSMTH(WRONG_COURSE);
-        }
-        model.setGroup(group, Integer.parseInt(c));
-
-    }
-
-    private void enterConnect(){
-        String s;
-        view.printSMTH(DOM_TEL);
-        while (!(s = sc.nextLine()).matches(DOM_TEL)) {
-            view.printSMTH(WRONG_DATA);
-        }
-        model.setDom_tel(s);
-
-        view.printSMTH(MOD_TEL);
-        while (!(s = sc.nextLine()).matches(MOD_TEL)){
-            view.printSMTH(WRONG_DATA);
-        }
-        model.setMod_tel(s);
-
-        view.printSMTH(ADD_MOD_TEL);
-        while (!((s = sc.nextLine()).matches(MOD_TEL) || s.equals("-"))){
-            view.printSMTH(WRONG_DATA);
-        }
-        model.setMod_tel_add(s);
-
-        view.printSMTH(EMAIL);
-        while (!(s = sc.nextLine()).matches(EMAIL)){
-            view.printSMTH(WRONG_DATA);
-        }
-        model.setEmail(s);
-
-        view.printSMTH(SKYPE);
-        while (!(s = sc.nextLine()).matches(SKYPE)){
-            view.printSMTH(WRONG_DATA);
-        }
-        model.setSkype(s);
-    }
-
-    private void enterAddress(){
-        String index, city, street, dom, kv;
-
-        view.printSMTH(INDEX);
-        while (!(index = sc.nextLine()).matches(INDEX)) {
-            view.printSMTH(WRONG_DATA);
-        }
-
-        view.printSMTH(CITY);
-        while (!(city = sc.nextLine()).matches(NAMES)) {
-            view.printSMTH(WRONG_DATA);
-        }
-
-        view.printSMTH(STREET);
-        while (!(street = sc.nextLine()).matches(NAMES)) {
-            view.printSMTH(WRONG_DATA);
-        }
-
-        view.printSMTH(DOM);
-        while (!(dom = sc.nextLine()).matches(DOM)) {
-            view.printSMTH(WRONG_DATA);
-        }
-
-        view.printSMTH(APART);
-        while (!((kv = sc.nextLine()).matches(KV) || kv.equals("-"))) {
-            view.printSMTH(WRONG_DATA);
-        }
-
-        model.setAddress(index,city,street,dom,kv);
-    }
-*/
 }
